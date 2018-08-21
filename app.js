1b359a16fdf3d980b9c2d8111bf43a5cfcad8de1
mjhmjh1104 <mjhmjh1104@naver.com>
2018-08-21 20:19:46 +0900

    installed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var path = require('path');
 
-app.set('view engine', 'ejs');
-app.use(require('express').static(path.join(__dirname, 'public')));
+//app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT | 3000, function() {
   console.log('Server on');
