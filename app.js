68f7cd4685a40eba6dbb27f63bb9143d476550f1
mjhmjh1104 <mjhmjh1104@naver.com>
2018-08-21 20:26:42 +0900

    changed

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('main');
+});
 
 app.listen(process.env.PORT | 3000, function() {
   console.log('Server on');
