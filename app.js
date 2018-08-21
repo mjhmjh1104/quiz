e3cf33f7e632c01c2ad058a50f8ef17c3912ed7e
mjhmjh1104 <mjhmjh1104@naver.com>
2018-08-21 20:33:37 +0900

    debugging

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('main');
