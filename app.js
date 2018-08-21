94ba63afaaf3458ab02f5c76bf05ecbb4cc05105
mjhmjh1104 <mjhmjh1104@naver.com>
2018-08-21 23:04:31 +0900

    updating

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.send('Test');
+  res.render('main');
 });
 
 app.listen(process.env.PORT || 3000, function() {
